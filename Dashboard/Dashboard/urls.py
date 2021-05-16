611917022fd2c74f23a7cf6001a99e6e20fcdd00
Udayan Walvekar <rujul.w@gmail.com>
2021-05-17 00:45:33 +0530

    add-user-to-check-migrations-

--- Dashboard/Dashboard/urls.py
+++ Dashboard/Dashboard/urls.py
@@ -20,7 +20,6 @@ from django.urls import path
 from dash_app import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('homepage/', include('dash_app.urls')),
     path('admin/', admin.site.urls),
     path('', views.signin, name='sign-in'),
