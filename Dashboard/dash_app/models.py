0b0658e784ecefb3a9ff13b17a2a4b667ccf86ac
Udayan Walvekar <rujul.w@gmail.com>
2021-07-01 23:30:53 +0530

    update-model-fields#2

--- Dashboard/dash_app/models.py
+++ Dashboard/dash_app/models.py
@@ -30,20 +30,27 @@ class Patient(models.Model):
     state = models.CharField(max_length=100, default='Maharashtra')
 
     address = models.CharField(max_length=500, default="Default address")
+    allergies = models.CharField(max_length=500, default="Dust Allergy")
+    medications = models.CharField(max_length=500, default="Cetrizine")
+    observations = models.CharField(max_length=500, default="Calcium Deficit")
     blood_type = models.CharField(max_length=5, default='b +ve')
     hospital_name = models.CharField(max_length=50, default='DMH')
     visit_type = models.CharField(max_length=50, default='Consult')
     last_interaction = models.DateField(default=datetime.now())
 
     @classmethod
-    def create(cls, name, gender, date_of_birth,address, email, phone_number, birth_place, country, state):
+    def create(cls, name, gender, date_of_birth,address,allergies,medications,observations, email, phone_number, hospital_name, birth_place, country, state):
         patient = cls(
             name=name,
             gender=gender,
             date_of_birth=date_of_birth,
             address=address,
+            allergies=allergies,
+            medications=medications,
+            observations=observations,
             email=email,
             phone_number=phone_number,
+            hospital_name=hospital_name,
             birth_place=birth_place,
             country=country,
             state=state
